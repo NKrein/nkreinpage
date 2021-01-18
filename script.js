6d1fc03dadbdd805265e75fd5672988d24f45133
Nicolás Krein Werle <nikokrein@gmail.com>
2021-01-18 15:30:44 -0300

    Se agrego imagenes e iconos para incorporar al contenido, menu lateral a mejorar y divison de secciones propias y de la pagina

---
+++ script.js
@@ -0,0 +1,34 @@
+//------------------------------------------------------SideBar-----------------------------------
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "transpatent";
+  }
+
+//---------------------------------------------------------Stiky Boton----------------------------
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar-btn");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}

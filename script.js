d4c4504747d485659b3f9982b73aa30d50e385ad
Nicolás Krein Werle <nikokrein@gmail.com>
2021-01-26 21:11:49 -0300

    Se cambio el fondo y colores, se agrego transparencia al menu/header/footer. Boton de menu mejorado. Se agregaron imagenes

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 //------------------------------------------------------SideBar-----------------------------------
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("mySidenav").style.width = "300px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "transpatent";
+    document.body.style.backgroundColor = "white";
   }
 
 //---------------------------------------------------------Stiky Boton----------------------------

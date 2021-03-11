ad3d1d5159b7a8c78b229199a5686a47bf8a67fa
Nicolás Krein Werle <nikokrein@gmail.com>
2021-03-10 23:43:37 -0300

    Contactos y Links + Portfolio y trabajos + Modificacion en Servicios: Precios y re-text + Contenido pagina Profile y maquetacion final

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 //------------------------------------------------------SideBar-----------------------------------
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
     document.getElementById("mySidenav").style.width = "300px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
   }
   
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "white";
